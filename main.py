559e47904becbfb99f7b7195ebba79b1f2cea1e8
CodeMahmoud <mahmoudcode913@gmail.com>
2025-01-27 21:03:19 -0600

    added pygame libary and initialized it

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+from pygame import *
 import pygame
+from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print( "Starting asteroids!")
-
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    black = (0, 0, 0)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        display.flip()
 
 if __name__ == "__main__":
     main()
